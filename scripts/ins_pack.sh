cfb3c5d83d6057b49214c86e286e61c4d051e7d0
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-05-20 20:28:28 +0700

    Feat: Add scripts

---
+++ scripts/ins_pack.sh
@@ -0,0 +1,10 @@
+sudo apt-get install cmake -y
+sudo apt-get install zlib1g-dev -y
+sudo apt-get install libssl-dev -y
+# sudo apt-get install python3-dev -y
+sudo apt-get install libhwloc-dev -y
+sudo apt-get install libevent-dev -y
+sudo apt-get install language-pack-en -y
+sudo locale-gen en_US.UTF-8
+sudo update-locale LANG=en_US.UTF-8
+# source ~/.bashrc
